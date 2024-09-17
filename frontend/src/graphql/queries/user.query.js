2421554084d7957bba6cd1098d813ef7bcef16ee
pavankumart18 <pavankumart7052@gmail.com>
2024-09-18 05:04:30 +0530

    Added Get user and transactions using some relationships in graph ql

--- frontend/src/graphql/queries/user.query.js
+++ frontend/src/graphql/queries/user.query.js
@@ -10,3 +10,22 @@ export const GET_AUTHENTICATED_USER = gql`
         }
     }
 `;
+
+export const GET_USER_AND_TRANSACTIONS = gql`
+    query GetUserAndTransactions($userId: ID!) {
+        user(userId: $userId) {
+            _id
+            username
+            name
+            profilePicture
+            transactions {
+                _id
+                title
+                amount
+                category
+                type
+                createdAt
+            }
+        }
+    }
+`;
